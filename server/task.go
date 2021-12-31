53cd8532cbab2da2ceafb9958613740b40459541
flywaiting <flywaiting@hotmail.com>
2021-12-31 16:04:27 +0800

    cache

--- server/task.go
+++ server/task.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"lgc/util"
 	"os/exec"
-	"sync"
+	"time"
 )
 
 type Task struct {
@@ -16,87 +16,64 @@ type Task struct {
 	Rname  string
 	Cmds   string
 	cancel context.CancelFunc
-
-	nxt *Task
 }
 
-var tm sync.Mutex
+// 任务 channel
+var tch chan *Task
 
-// 新建任务
-func NewTask() {
-	if que.size >= limit {
-		return
-	}
+// 任务默认超时
+var tto time.Duration
 
-	t := &Task{}
-	t.Ctime = util.Tsp()
-	taskChan <- t
+func init() {
+	tto = 1 * time.Second
+	tch = make(chan *Task)
+	go gTask()
 }
 
-// 取消任务
-func CancelTask(tId int) {
-	t := que.find(tId)
-	if t == nil {
-		return
-	}
-	done(t, util.Cancel)
-}
-
-// 任务列表
-func TaskList() {
-
-}
-
-var taskChan chan *Task
-var que *queTask
-
-func TaskInit(ctx context.Context) {
-	taskChan = make(chan *Task, limit)
-	que = NewQue()
-	go shedule(ctx)
-}
-
-// 任务排班
-func shedule(ctx context.Context) {
-	for {
-		select {
-		case t := <-taskChan:
-			run(ctx, t)
-		case <-ctx.Done():
-			close(taskChan)
-			return
-		}
+func gTask() {
+	for t := range tch {
+		run(t)
 	}
 }
 
-func run(ctx context.Context, t *Task) {
-	c, cancel := context.WithCancel(ctx)
+func taskNext(t *Task) bool {
+	select {
+	case tch <- t:
+		return true
+	case <-time.After(tto):
+		return false
+	}
+}
 
+func run(t *Task) {
+	// todo	判定任务的有效性
+	
+	defer queNext(t)
+
+	ctx, cancel := context.WithCancel(context.TODO())
 	t.cancel = cancel
 	t.Stime = util.Tsp()
-	cmd := exec.CommandContext(c, "a")
-	// todo	输出设置
+	// todo	输出记录
+	// todo	任务执行, 走配置
+	cmd := exec.CommandContext(ctx, "c")
 	err := cmd.Run()
-
-	var status int
-	if err != nil {
-		status = util.Wrong
-	} else {
-		status = util.Done
+	if err != nil || (cmd.ProcessState != nil && cmd.ProcessState.ExitCode() != 0) {
+		// todo	task abort
+		return
 	}
-	done(t, status)
+	// todo finish
 }
 
 func done(t *Task, status int) {
-	tm.Lock()
-	defer tm.Unlock()
+	// tm.Lock()
+	// defer tm.Unlock()
 
-	if t == nil || t.Status != util.Ready {
-		return
-	}
+	// if t == nil || t.Status != util.Ready {
+	// 	return
+	// }
 
-	t.cancel()
-	t.Status = status
-	t.Etime = util.Tsp()
-	// todo 数据写入
+	// t.cancel()
+	// t.Status = status
+	// t.Etime = util.Tsp()
+	// // todo 数据写入
 }
